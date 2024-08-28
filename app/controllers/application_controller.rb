e37b3b8c6348caa8edc4aa0468721a8abe95f6bb
Jeferson Avliez <jeffersonm027@gmail.com>
2024-08-28 17:04:33 -0500

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,9 @@
+class ApplicationController < ActionController::Base
+  def hola
+    render html: "Y aserejé! ¡Ja! ¡De Jé!
+De jebe tude jebere sebiunouba
+¡Majabi an de bugui an de buididipí!"
+   
+  end 
+end
+
