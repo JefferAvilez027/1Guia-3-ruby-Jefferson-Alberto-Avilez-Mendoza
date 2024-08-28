f2f47e889ae2e3bf2294b210624e652f2e1f71fd
Jeferson Avliez <jeffersonm027@gmail.com>
2024-08-28 17:19:57 -0500

    first commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  def hola
+  def textoh
     render html: "Y aserejé! ¡Ja! ¡De Jé!
 De jebe tude jebere sebiunouba
 ¡Majabi an de bugui an de buididipí!"
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  root 'application#hola'
+  root 'application#textoh'
 
 end
